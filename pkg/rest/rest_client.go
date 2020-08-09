2486ff41fa3e5064d66f5020c618f243aecdde4e
Artem Barger <bartem@il.ibm.com>
2020-08-09 11:38:54 +0300

    Increase REST client HTTP connections pool
    This commit adjusts number of idle connections per host for connection
    pool from default to 100, so it can utilize full capacity of pool in
    case client connects to single server only.
    
    Signed-off-by: Artem Barger <bartem@il.ibm.com>

--- pkg/rest/rest_client.go
+++ pkg/rest/rest_client.go
@@ -7,8 +7,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.ibm.com/blockchaindb/library/pkg/constants"
@@ -35,7 +37,22 @@ func NewRESTClient(rawurl string) (*Client, error) {
 	if err != nil {
 		return nil, errors.Wrapf(err, "parsing url %s", rawurl)
 	}
-	res.httpClient = http.DefaultClient
+
+	res.httpClient = &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			ForceAttemptHTTP2:     true,
+			MaxIdleConns:          100,
+			MaxIdleConnsPerHost:   100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+	}
 	return res, nil
 }
 
