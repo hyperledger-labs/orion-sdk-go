1d8504d76092247cc8e59abcd4a8a4d4f705d6e5
senthil <cendhu@gmail.com>
2021-11-26 15:31:53 +0530

    demo: academic doc management
    Signed-off-by: senthil <cendhu@gmail.com>

---
+++ examples/academic-docs/proof-and-verification/proof-and-verification.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/hyperledger-labs/orion-sdk-go/examples/academic-docs/util"
+	"github.com/hyperledger-labs/orion-sdk-go/pkg/bcdb"
+	"github.com/hyperledger-labs/orion-server/pkg/types"
+)
+
+func main() {
+	db, err := util.CreateConnection()
+	if err != nil {
+		fmt.Errorf(err.Error())
+		return
+	}
+
+	session, err := util.OpenSession(db, "bob")
+	if err != nil {
+		fmt.Errorf(err.Error())
+		return
+	}
+
+	lgr, err := session.Ledger()
+	if err != nil {
+		fmt.Errorf(err.Error())
+		return
+	}
+
+	jsonReceipt, err := ioutil.ReadFile("../prepare-data/person0.txt")
+	if err != nil {
+		fmt.Errorf(err.Error())
+		return
+	}
+
+	txReceipt := &types.TxReceipt{}
+	if err = json.Unmarshal(jsonReceipt, txReceipt); err != nil {
+		fmt.Errorf(err.Error())
+		return
+	}
+
+	ms := `{"roll_number":0,"physics":100,"chemistry":100,"mathematics":100,"pass":true,"year":2010}`
+	markSheetHash, err := bcdb.CalculateValueHash("db1", "person0", []byte(ms))
+	if err != nil {
+		fmt.Errorf(err.Error())
+		return
+	}
+
+	proof, err := lgr.GetDataProof(txReceipt.Header.BaseHeader.Number, "db1", "person0", false)
+	if err != nil {
+		fmt.Errorf(err.Error())
+		return
+	}
+
+	fmt.Println(ms)
+	isSuccess, err := proof.Verify(markSheetHash, txReceipt.Header.StateMerkelTreeRootHash, false)
+	if isSuccess {
+		fmt.Println("This marksheet is valid")
+	} else {
+		fmt.Println("This marksheet is invalid")
+	}
+
+	ms = `{"roll_number":0,"physics":90,"chemistry":100,"mathematics":100,"pass":true,"year":2010}`
+	markSheetHash, err = bcdb.CalculateValueHash("db1", "person0", []byte(ms))
+	if err != nil {
+		fmt.Errorf(err.Error())
+		return
+	}
+
+	fmt.Println(ms)
+	isSuccess, err = proof.Verify(markSheetHash, txReceipt.Header.StateMerkelTreeRootHash, false)
+	if isSuccess {
+		fmt.Println("marksheet is valid")
+	} else {
+		fmt.Println("marksheet is invalid")
+	}
+}
---
+++ examples/academic-docs/util/util.go
@@ -0,0 +1,63 @@
+package util
+
+import (
+	"time"
+
+	"github.com/hyperledger-labs/orion-sdk-go/pkg/bcdb"
+	"github.com/hyperledger-labs/orion-sdk-go/pkg/config"
+	"github.com/hyperledger-labs/orion-server/pkg/logger"
+)
+
+func CreateConnection() (bcdb.BCDB, error) {
+	logger, err := logger.New(
+		&logger.Config{
+			Level:         "debug",
+			OutputPath:    []string{"stdout"},
+			ErrOutputPath: []string{"stderr"},
+			Encoding:      "console",
+			Name:          "bcdb-client",
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	conConf := &config.ConnectionConfig{
+		ReplicaSet: []*config.Replica{
+			{
+				ID:       "orion-server1",
+				Endpoint: "http://127.0.0.1:6001",
+			},
+		},
+		RootCAs: []string{
+			"../crypto/CA/CA.pem",
+		},
+		Logger: logger,
+	}
+
+	db, err := bcdb.Create(conConf)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func OpenSession(db bcdb.BCDB, userID string) (bcdb.DBSession, error) {
+	sessionConf := &config.SessionConfig{
+		UserConfig: &config.UserConfig{
+			UserID:         userID,
+			CertPath:       "../crypto/" + userID + "/" + userID + ".pem",
+			PrivateKeyPath: "../crypto/" + userID + "/" + userID + ".key",
+		},
+		TxTimeout:    20 * time.Second,
+		QueryTimeout: 10 * time.Second,
+	}
+
+	session, err := db.Session(sessionConf)
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
