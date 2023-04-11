ea98c50fc08c0462cf782c62440f9a84e081e00d
Liran Funaro <liran.funaro@gmail.com>
2023-04-11 11:16:54 +0300

    Test if port available before using it for testing.
    Signed-off-by: Liran Funaro <liran.funaro@gmail.com>

--- internal/test/ports.go
+++ internal/test/ports.go
@@ -3,20 +3,38 @@
 
 package test
 
-import "sync"
+import (
+	"fmt"
+	"net"
+	"sync"
+)
 
 var portMutex sync.Mutex
 var nodePortBase uint32 = 32000
 var peerPortBase uint32 = 33000
 
+const PortLimit = 1 << 16
+
+func testPort(port uint32) bool {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	_ = l.Close()
+	return err == nil
+}
+
 func GetPorts() (nodePort, peerPort uint32) {
 	portMutex.Lock()
 	defer portMutex.Unlock()
 
-	nodePort = nodePortBase
-	peerPort = peerPortBase
-	nodePortBase++
-	peerPortBase++
+	for nodePortBase < PortLimit && peerPortBase < PortLimit {
+		nodePort = nodePortBase
+		peerPort = peerPortBase
+		nodePortBase++
+		peerPortBase++
 
-	return
+		if testPort(nodePort) && testPort(peerPort) {
+			return
+		}
+	}
+
+	panic("Could not find available port for testing")
 }
